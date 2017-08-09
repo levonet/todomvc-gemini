74b0296fbc4f7129f6012871b1dd4c1571669f8c
Pavel Bashinsky <pbashinsky@yandex-team.ru>
2017-08-09 12:03:05 +0300

    Сохраняем изображения в LFS

--- .gemini.js
+++ .gemini.js
@@ -3,6 +3,7 @@
 module.exports = {
     rootUrl: 'http://localhost:8080/',
     gridUrl: 'http://localhost:4444/wd/hub',
+    screenshotsDir: 'test/gemini/references',
 
     // sessionsPerBrowser: 3,
     // retry: 2,
